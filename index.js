9c05bf73d3b4c30d3b264885343f98fa8d5b5d60
jeheald23 <jeheald23@gmail.com>
2023-12-04 20:47:46 +0100

    added packages and content

--- index.js
+++ index.js
@@ -1,75 +1,201 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const uuid = require('uuid');
+
 const app = express();
-morgan = require('morgan');
+
+app.use(bodyParser.json());
 
 const port = 8080;
 
-let favoriteMovies = [
+let users = [
+  {
+    id: 1,
+    name: 'Bob Smith',
+    username: 'bobsmith',
+    favorites: ['Blade Runner', 'A Clockwork Orange', 'Stalker']
+  }
+];
+
+let movies = [
     {
       title: 'Blade Runner',
-      director: 'Ridley Scott'
+      director: 'Ridley Scott',
+      genre: 'Science Fiction',
+      description: 'some text here',
+      image: 'some image here',
+      featured: true
     },
     {
       title: 'A Clockwork Orange',
-      director: 'Stanley Kubrick'
+      director: 'Stanley Kubrick',
+      genre: 'Science Fiction',
+      description: 'some text here',
+      image: 'some image here',
+      featured: false
     },
     {
       title: 'Brazil',
-      director: 'Terry Gilliam'
+      director: 'Terry Gilliam',
+      genre: 'Science Fiction',
+      description: 'some text here',
+      image: 'some image here',
+      featured: true
     },
     {
       title: '8 1/2',
-      director: 'Federico Fellini'
+      director: 'Federico Fellini',
+      genre: 'Drama',
+      description: 'some text here',
+      image: 'some image here',
+      featured: true
     },
     {
       title: 'Mulholland Drive',
-      director: 'David Lynch'
-    },
-    {
-      title: 'Excalibur',
-      director: 'John Boorman'
-    },
-    {
-      title: 'Alphaville',
-      director: 'Jean-Luc Godard'
-    },
-    {
-      title: 'L\'Avventura',
-      director: 'Michelangelo Antonioni'
-    },
-    {
-      title: 'My Dinner with Andre',
-      director: 'Louis Malle'
-    },
-    {
-      title: 'Stalker',
-      director: 'Andrei Tarkovsky'
+      director: 'David Lynch',
+      genre: 'Drama',
+      description: 'some text here',
+      image: 'some image here',
+      featured: false
     }
   ];
 
-app.use(morgan('common'));
+  let directors = [
+    {
+      name: 'Michelangelo Antonioni',
+      bio: "[A]n Italian director and filmmaker… best known for his 'trilogy on modernity and its discontents'—L'Avventura (1960), La Notte (1961), and L'Eclisse (1962)—as well as the English-language film Blowup (1966). His films have been described as 'enigmatic and intricate mood pieces' that feature elusive plots, striking visual composition, and a preoccupation with modern landscapes. His work substantially influenced subsequent art cinema. Antonioni received numerous awards and nominations throughout his career, being the only director to have won the Palme d'Or, the Golden Lion, the Golden Bear and the Golden Leopard. (from Wikipedia)",
+      birth: '1912',
+      death: '2007'
+    },
+    {
+      name: 'Federico Fellini',
+      bio:  'some text here',
+      birth: '1920',
+      death: '1993'
+    }
+  ];
 
-app.get('/', (req, res, next) => {
-res.send('Welcome to my myFlix movie app!');
+//get a list of all users
+app.get('/users', (req, res) => {
+  res.json(users);
 });
 
-
-app.get('/movies', (req, res, next) => {
-res.json(favoriteMovies);
+//register a user
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+  if (!newUser.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
 });
 
-app.get('/secreturl', (req, res, next) => {
-    res.send('Why are you here?');
+//get a user by username
+app.get('/users/:username', (req, res) => {
+  res.json(users.find((user) =>
+  { return user.username === req.params.username }));
+});
+
+//update a user's username by username
+app.put('/users/:username', (req, res) => {
+  let user = users.find((user) => { return user.username === req.params.username });
+  if (user) {
+    user.username = req.body.username;
+    res.status(200).send(user);
+  } else {
+    res.status(404).send('User not found.');
+  }
+});
+
+//get a list of a user's favorite movies
+app.get('/users/:username/favorites', (req, res) => {
+  res.json(users.find((user) =>
+  { return user.username === req.params.username }).favorites);
+}); 
+
+//add a movie to a user's list of favorites
+app.post('/users/:username/favorites/:movie', (req, res) => {
+  let user = users.find((user) => { return user.username === req.params.username });
+  if (user) {
+    user.favorites.push(req.params.movie);
+    res.status(200).send(user);
+  } else {
+    res.status(404).send('User not found.');
+  }
+}); 
+
+//delete a movie from a user's list of favorites  
+app.delete('/users/:username/favorites/:movie', (req, res) => {
+  let user = users.find((user) => { return user.username === req.params.username });
+  if (user) {
+    user.favorites = user.favorites.filter((value) => { return value !== req.params.movie });
+    res.status(200).send(user);
+  } else {
+    res.status(404).send('User not found.');
+  }
+});
+
+//delete a user by userID
+app.delete('/users/:userID', (req, res) => {
+  let user = users.find((user) => { return user.id === req.params.userID });
+  if (user) {
+    users = users.filter((value) => { return value !== user });
+    res.status(200).send('User ' + req.params.userID + ' was deleted.');
+  } else {
+    res.status(404).send('User not found.');
+  }
+});  
+
+
+//get a list of all movies
+app.get('/movies', (req, res) => {
+res.json(movies);
+});
+
+//get data about a single movie by title
+app.get('/movies/:title', (req, res) => {
+res.json(movies.find((movie) =>
+{ return movie.title === req.params.title }));
+});
+
+//get a list of all genres
+app.get('/movies/genres', (req, res) => {
+  res.json(movies.map((movie) => { return movie.genre }));
   });
 
-app.use(express.static('public'));
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Danger Will Robinson!');
+//get a list of movies by genre
+app.get('/movies/genres/:genre', (req, res) => {
+  res.json(movies.filter((movie) =>
+  { return movie.genre === req.params.genre }));
   });
+
+//get genre by movie title
+app.get('/movies/:title/genre', (req, res) => {
+  res.json(movies.find((movie) =>
+  { return movie.title === req.params.title }).genre);
+  });
+
+//get data about a movie by director
+app.get('/movies/directors/:name', (req, res) => {
+  res.json(movies.find((movie) =>
+  { return movie.director === req.params.name }));
+  });
+
+//get a list of all directors
+  app.get('/directors', (req, res) => {
+    res.json(directors);
+    });  
+
+//get data about a director by name
+app.get('/directors/:name', (req, res) => {
+  res.json(directors.find((director) =>
+  { return director.name === req.params.name }));
+  });
+
   
-
 app.listen(port, () => {
 console.log(`Your app is listening on port ${port}.`);
 });
