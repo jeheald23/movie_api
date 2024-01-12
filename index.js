71719d03bb4cc44acc2b3257d64d2e766d3d876a
jeheald23 <jeheald23@gmail.com>
2024-01-12 11:54:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -218,8 +218,7 @@ async (req, res) => {
 });
 
 //get a list of all movies
-app.get("/movies", passport.authenticate("jwt", { session: false }),
-async (req, res) => {
+app.get("/movies", async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
