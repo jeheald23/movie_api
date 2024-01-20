f6cd8762463451ab101c8daf406b0c91a48df71f
jeheald23 <jeheald23@gmail.com>
2024-01-20 13:51:22 +0100

    fixed cors issues

--- index.js
+++ index.js
@@ -38,22 +38,6 @@ app.get('/', (req, res) => {
 });
 
 let allowedOrigins = "*";
-//allow specific set of origins to access your API
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn't found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn't allow access from origin" +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
 
 app.use(cors());
 
